36ba14db5075f089d19dee8c468a1cd45f65e661
Nitesh6206 <niteshsingh6206@example.com>
2025-01-12 18:38:37 +0530

    minor changes

--- Backend/Middleware/auth.js
+++ Backend/Middleware/auth.js
@@ -1,24 +1,18 @@
-// middleware/auth.js
 const jwt = require('jsonwebtoken');
 
-module.exports = function (req, res, next) {
-    // Get token from header
-    const authHeader = req.header('Authorization');
-    if (!authHeader) {
-        return res.status(401).json({ msg: 'No token, authorization denied' });
-    }
-
-    const token = authHeader.split(' ')[1]; // Assuming "Bearer TOKEN"
-
-    if (!token) {
-        return res.status(401).json({ msg: 'No token, authorization denied' });
-    }
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded.userId;
-        next();
-    } catch (err) {
-        res.status(401).json({ msg: 'Token is not valid' });
-    }
+const auth = (req, res, next) => {
+  // Middleware logic to verify the JWT token
+  const token = req.header('x-auth-token');
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
 };
+
+module.exports = auth;
